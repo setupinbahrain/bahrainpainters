fdce8f3b4d86995bd8c67d2c877f82a6e2b7ebfd
Setup in Bahrain <setupinbahrain@Setups-MacBook-Pro.local>
2026-04-14 09:24:48 +0400

    Fix 404s by expanding NextIntl middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,5 @@ export default createMiddleware(routing);
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(ar|en)/:path*']
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
